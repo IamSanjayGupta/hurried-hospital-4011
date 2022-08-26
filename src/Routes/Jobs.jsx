bc59b543d575d1832f4857ec50f39c39e0e1672f
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-08-27 00:49:21 +0530

    Signing Off for day-4. Day was 7-% productive

--- src/Routes/Jobs.jsx
+++ src/Routes/Jobs.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import SearchInput from "../components/SearchInput";
 import FooterNormal from "../components/FooterNormal";
 import FilterButton from "../components/FilterButton";
-import { Box, Container, Divider, Flex, VStack } from "@chakra-ui/react";
+import { Box, Container, Divider, Flex, Text, VStack } from "@chakra-ui/react";
 import { getJobsApi } from "../utils/api";
 import { capitalize, getJobBullets } from "../utils/polyfills";
 import { AppContext } from "../context/AppContext";
@@ -36,14 +36,51 @@ const Jobs = () => {
         <FilterButton data={data} />
       </Container>
       <Divider bg={"gray.300"} my="5" />
-      <Container maxW={"container.lg"} position="sticky">
+      <Container maxW={"container.lg"} position="relative">
         <Flex gap={5}>
           <VStack w={{ base: "100%", md: "50%" }}>
             <JobCards />
             <JobCards />
             <JobCards />
+            <JobCards />
+            <JobCards />
+            <JobCards />
+            <JobCards />
+            <JobCards />
+            <JobCards />
+            <JobCards />
+            <JobCards />
+            <JobCards />
           </VStack>
-          <VStack w={{ base: "0%", md: "50%" }} bg={"blue"}></VStack>
+          <VStack
+            w={"50%"}
+            rounded="md"
+            display={{ base: "none", md: "block" }}
+            position="sticky"
+            top={5}
+            height={"fit-content"}
+            boxShadow="rgba(0, 0, 0, 0.82) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px"
+            p="3"
+          >
+            <Text>
+              Roles and Responsibilities: Build efficient, testable, and reusable PHP modules and
+              plugins Integration of user-facing elements developed by back-end developers Solve
+              complex performance problems and architectural challenges Ensure to resolve identified
+              issues related to PHP development to different customers varying from senior managers
+              to varied technical personnel. Maintain and manage clear plus complete documentation.
+              Write all clean object-oriented PHP as well as efficient SQL. Develop and deploy new
+              features, modules and write bug-free code for web-based applications. Key Skills:
+              Proficiency in PHP programming with OOPS, MySQL, JavaScript, and jQuery. Understanding
+              of MVC design patterns Hands-on experience in designing complex database queries with
+              a multi DB/table structure Hands-on experience in back-end technologies, such as HTML
+              and CSS Understanding of Browser compatibilities and standard Compliances Good
+              knowledge of open-source CMS such as WordPress, Laravel, Magento, etc will be an added
+              advantage. Experience in core php: 3 years to 6 years Job Types: Full-time, Walk-In
+              Salary: ₹40,000.00 - ₹70,000.00 per month Benefits: Paid sick time Schedule: Day shift
+              Monday to Friday Supplemental Pay: Performance bonus Work Remotely: No", "job_type":
+              "Full-time",
+            </Text>
+          </VStack>
         </Flex>
       </Container>
 
